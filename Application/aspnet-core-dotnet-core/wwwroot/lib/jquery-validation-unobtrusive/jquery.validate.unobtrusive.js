e5a8a1a7274c48f483ca3e295d8a504758f26c60
Pratik Patni <49429456+PratikPatni@users.noreply.github.com>
2022-05-16 19:02:18 +0530

    Update jquery.validate.unobtrusive.js

--- Application/aspnet-core-dotnet-core/wwwroot/lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js
+++ Application/aspnet-core-dotnet-core/wwwroot/lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js
@@ -66,7 +66,7 @@
         }
     }
 
-    function onErrors(event, validator) {  // 'this' is the form element
+    function onErrors(_event, validator) {  // 'this' is the form element
         var container = $(this).find("[data-valmsg-summary=true]"),
             list = container.find("ul");
 
@@ -96,7 +96,7 @@
         }
     }
 
-    function onReset(event) {  // 'this' is the form element
+    function onReset(_event) {  // 'this' is the form element
         var $form = $(this),
             key = '__jquery_unobtrusive_validation_form_reset';
         if ($form.data(key)) {
